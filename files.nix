52937bc08d44d1256aa40c256f657140d5a683d3
Andr Vo <av@gyrus.biz>
2024-12-17 10:07:48 +0200

    ssh auth sock for zellij

--- files.nix
+++ files.nix
@@ -27,6 +27,13 @@ in
 			else
 	        export SSH_AUTH_SOCK="/tmp/ssh-agent-$USER-tmux"
 			fi
+
+			if [ "X$ZELLIJ" = "X" ];
+			then
+	        test $SSH_AUTH_SOCK && ln -sf "$SSH_AUTH_SOCK" "/tmp/ssh-agent-$USER-zellij"
+			else
+	        export SSH_AUTH_SOCK="/tmp/ssh-agent-$USER-zellij"
+			fi
 		'';
 	};
 
